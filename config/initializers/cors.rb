9603fef2ae9557c81d056b3635af15db397a2269
Mitsukiooka <mooka0907@gmail.com>
2025-05-17 15:03:13 +1200

    Update cors.rb

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -2,7 +2,7 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     origins 'http://localhost:3001'  # Next.js のURL
     resource '*',
-      headers: :any,
-      methods: [:get, :post, :patch, :put, :delete, :options]
+             headers: :any,
+             methods: [:get, :post, :patch, :put, :delete, :options]
   end
 end
